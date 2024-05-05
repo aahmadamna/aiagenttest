612aa4a66d52ac3760fc4c2fc4e6a90b8f3b51a8
aahmadamna <166873538+aahmadamna@users.noreply.github.com>
2024-05-05 15:28:27 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,5 @@
 import './styles.css';
 import Link from 'next/link';
-import { useState } from 'react';
-import CommandInput from './CommandInput';
-function MyApp({ Component, pageProps }) {
-   const [command, setCommand] = useState('');
-
-    // Function to handle the command input
-    const handleCommandInput = (event) => {
-        setCommand(event.target.value);
-    };
-
-    // Function to submit the command
-    const submitCommand = () => {
-        // Implement logic to process the command
-        console.log('Command submitted:', command);
-        // You can perform actions based on the command here
-        // For now, let's just clear the command input
-        setCommand('');
-    };
-
-  return (
     <div>
       <header>
         <div className="container">
@@ -44,25 +24,15 @@ function MyApp({ Component, pageProps }) {
       </div>   
       <div className="container">
         <div className="search-bar">
-             {/* Command input field */}
-                    <input
-                        type="text"
-                        value={command}
-                        onChange={handleCommandInput}
-                        placeholder="Enter command..."
-                    />
-                    {/* Button to submit the command */}
-                    <button onClick={submitCommand}>Submit</button>
-                </div>
-            </div>
-            <div className="container page-content">
-                {/* Render the component */}
-                <Component {...pageProps} />
-            </div>
+             <input type="text" placeholder="How can I help you?" />
         </div>
-    );
+      </div>
+      <div className="container page-content">
+        <Component {...pageProps} />
+      </div>   
+    </div>
+  );
 }
-
 export default MyApp;
 
 
